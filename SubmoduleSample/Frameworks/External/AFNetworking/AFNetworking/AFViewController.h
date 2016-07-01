47cf3195b0abec71d22838ef0f20e63fa69c624e
Geass <mingjieding@linkdoc.com>
2016-07-01 10:44:04 +0800

    first step

---
+++ SubmoduleSample/Frameworks/External/AFNetworking/AFNetworking/AFViewController.h
@@ -0,0 +1,13 @@
+//
+//  AFViewController.h
+//  AFNetworking
+//
+//  Created by 丁明杰 on 6/30/16.
+//  Copyright © 2016 AFNetworking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFViewController : UIViewController
+
+@end
--- SubmoduleSample/SubmoduleSample/ViewController.m
+++ SubmoduleSample/SubmoduleSample/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "MyViewController.h"
+#import "AFHTTPSessionManager.h"
+#import "AFViewController.h"
 
 @interface ViewController ()
 
@@ -17,6 +20,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    btn.backgroundColor = [UIColor blackColor];
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] init];
+}
+
+- (void)btnClick:(id)sender
+{
+//    MyViewController *vc = [[MyViewController alloc] init];
+    AFViewController *vc = [[AFViewController alloc] init];
+    [self presentViewController:vc animated:YES completion:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
